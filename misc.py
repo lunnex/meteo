943ed23e57f72390ab97beb8135722d0fc92ea94
Korotkikh Ilya <65455065+lunnex@users.noreply.github.com>
2022-03-12 02:20:06 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -2,7 +2,6 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-
-bot = Bot(token = '1696258199:AAEZEk3o7B00zi7mW-MALdBrBomiFZcuq2U')
+bot = Bot(token = "")
 dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
